3b63151ac6c8389578c2711fdfb3c1f4d86b068a
Elison <1248647469@qq.com>
2020-03-30 20:53:44 +0800

    排ming面经

--- miniprogram/pages/ranking/ranking.js
+++ miniprogram/pages/ranking/ranking.js
@@ -5,62 +5,114 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    list: [{
+        name: "旺旺",
+        Fraca: "113",
+        classfi: "0",
+      url: "http://owemuck.kuai369.com/img/qcsmimg/%5Bqicai%5D1~78.jpg"
+      },
+      {
+        name: "旺旺1",
+        Fraca: "113",
+        classfi: "0",
+        url: "http://owemuck.kuai369.com/img/qcsmimg/%5Bqicai%5D1~78.jpg"
+      }, {
+        name: "旺旺2",
+        Fraca: "113",
+        classfi: "0",
+        url: "http://owemuck.kuai369.com/img/qcsmimg/%5Bqicai%5D1~78.jpg"
+      }, {
+        name: "旺旺3",
+        Fraca: "113",
+        classfi: "0",
+        url: "http://owemuck.kuai369.com/img/qcsmimg/%5Bqicai%5D1~78.jpg"
+      }, {
+        name: "旺旺4",
+        Fraca: "113",
+        classfi: "0",
+        url: "http://owemuck.kuai369.com/img/qcsmimg/%5Bqicai%5D1~78.jpg"
+      }, {
+        name: "旺旺5",
+        Fraca: "113",
+        classfi: "0",
+        url: "http://owemuck.kuai369.com/img/qcsmimg/%5Bqicai%5D1~78.jpg"
+      }, {
+        name: "旺旺6",
+        Fraca: "113",
+        classfi: "0",
+        url: "http://owemuck.kuai369.com/img/qcsmimg/%5Bqicai%5D1~78.jpg"
+      }, {
+        name: "旺旺7",
+        Fraca: "113",
+        classfi: "0",
+        url: "http://owemuck.kuai369.com/img/qcsmimg/%5Bqicai%5D1~78.jpg"
+      }, {
+        name: "旺旺",
+        Fraca: "113",
+        classfi: "0",
+        url: "http://owemuck.kuai369.com/img/qcsmimg/%5Bqicai%5D1~78.jpg"
+      }, {
+        name: "旺旺",
+        Fraca: "113",
+        classfi: "0",
+        url: "http://owemuck.kuai369.com/img/qcsmimg/%5Bqicai%5D1~78.jpg"
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
